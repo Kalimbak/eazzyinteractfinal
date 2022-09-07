cd6bc97023b60ae9ec1669765f72894289b1147e
kevin kalimba <95082403+Kalimbak@users.noreply.github.com>
2022-09-07 19:53:27 +0200

    Add files via upload

---
+++ eazzy-interact project/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+// import Navigation from "./components/Navigation";
+// import LeftSidebar from "./components/LeftSidebar";
+// import Content from "./components/Content";
+// import RightSidebar from "./components/RightSidebar";
+// import { useState } from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/home";
+import Login from "./components/LoginPage";
+
+function App() {
+  // const [mobileMenu, setmobileMenu] = useState(false);
+
+  // const toggle = () => {
+  //   setmobileMenu(!mobileMenu);
+  // };
+
+  return (
+    <>
+
+      {/* <Navigation openMenu={toggle} />
+
+      <main className="main-container">
+        <LeftSidebar burgerMenu={mobileMenu} closeMenu={toggle} />
+        <Content />
+        <RightSidebar />
+      </main> */}
+      <Router>
+        <Routes>
+        <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ eazzy-interact project/src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+    
+    return(
+       <div >
+       <a href="/home">
+login
+       </a>
+       </div>
+    )
+}
+export default Login;
